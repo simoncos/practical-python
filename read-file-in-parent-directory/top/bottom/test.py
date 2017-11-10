fa4e1f1016d0bf432872aeebbdc38bfb18f09f7e
Simon Zhao <l.z.simon@hotmail.com>
2017-11-10 15:56:45 +0800

    update

--- read-file-in-parent-directory/top/bottom/test.py
+++ read-file-in-parent-directory/top/bottom/test.py
@@ -1,10 +1,9 @@
 from os.path import dirname, abspath
 import os
 
-print('test results:')
-print(__file__)
+print('__file__:',__file__)
+print('current working directory:', os.getcwd()) # 打印当前工作目录
 print(dirname(__file__))
 print(abspath(__file__))
 print(dirname(abspath(__file__)))
 print(dirname(dirname(abspath(__file__))))
-print('current working directory:', os.getcwd()) # 打印当前工作目录
