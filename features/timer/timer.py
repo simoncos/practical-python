ac5b3ad55a1fc5937ffb2a87f59b0594cf703411
Simon Zhao <l.z.simon@hotmail.com>
2018-10-30 12:40:33 +0800

    add ContextDecorator method

--- features/timer/timer.py
+++ features/timer/timer.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from contextlib import ContextDecorator
 
 def timer(func):
     '''Function Level Timer via Decorator'''
@@ -29,6 +30,17 @@ def timer_(func):
         return result
     return timed
 
+class timer_elegant(ContextDecorator):
+    '''Elegant Timer via ContextDecorator'''
+    def __init__(self, name):
+        self.name = name
+    def __enter__(self):
+        self.start = datetime.now()
+    def __exit__(self, *args):
+        self.end = datetime.now()
+        self.elapse = (self.end - self.start).total_seconds()
+        print("Processing time for {} is: {} seconds".format(self.name, self.elapse))
+
 # Tests
 
 @timer
@@ -50,7 +62,21 @@ def test_3(a):
     a *= 2
     return a
 
+@timer_elegant('test_4')
+def test_4(a):
+    a *= 2
+    return a
+
+def test_5(a):
+    a *= 2
+    return a
+
 if __name__ == '__main__':
     print(test_1(1))
     print(test_2(2))
     print(test_3(3))
+    print(test_4(4))
+
+    with timer_elegant('test_5'):
+        result_5 = test_5(5)
+    print(result_5)
